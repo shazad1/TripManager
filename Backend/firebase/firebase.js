d5331ff26a561c5caecbc34231dbd3285f16df40
Shazad1 <shahzadhere@gmail.com>
2023-09-05 18:24:50 +1000

    commit working

--- Backend/firebase/firebase.js
+++ Backend/firebase/firebase.js
@@ -4,8 +4,15 @@ import 'firebase/firestore';
 import "firebase/database";
 import "firebase/storage";
 
-import firebaseConfig from './config';
-
+var firebaseConfig = {
+    apiKey: "AIzaSyCL45mwFtsS6K-U3U-RwpStF0-xh9AMGUM",
+    authDomain: "tawtripmanager.firebaseapp.com",
+    databaseURL: "https://tawtripmanager-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "tawtripmanager",
+    storageBucket: "tawtripmanager.appspot.com",
+    messagingSenderId: "445361061630",
+    appId: "1:445361061630:web:b7c84c08577902e9226faf"
+    };
 
 
 class Firebase {
